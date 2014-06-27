3c930a1fad96d35440f83174601165d21bacabfa
awkisopen <awkisopen@shutupandwrite.net>
2014-06-27 17:57:29 -0400

    Parser: Increase parsing efficiency; add attributes
    This commit introduces two new attributes, game_id and line_number, to
    generated Event objects. Between the game_id, a unique hash of all the
    game's lines, and the line_number, the position of each Event in the
    game log, all events can be uniquely identified. This change is
    necessary for database storage and retrieval to be possible.

--- lib/australium/parser.rb
+++ lib/australium/parser.rb
@@ -8,21 +8,24 @@ module Australium
     # @param [Array<String>] lines the lines to parse
     # @return [Array<Game>] the game(s) data
     def self.parse(lines)
-      games = [] ; state = GameState.new ; events = []
+      games = []
 
-      lines.each_with_index do |line, index|
-        event = parse_line(line, state)
+      game_lines = lines.slice_before(MapLoad::LOG_REGEX).select { |l| l.first =~ MapLoad::LOG_REGEX }
 
-        if event.is_a?(MapLoad) || index == lines.count - 1
-          games << Game.new(events) unless events.empty?
-          events = []
-          event = parse_line(line, GameState.new) # Parse a second time with the correct GameState
+      game_lines.each do |lines|
+        state = GameState.new
+        state.game_id = lines.hash
+        events = []
+
+        lines.each_with_index do |line, index|
+          event = parse_line(line, index, state)
+          unless event.nil?
+            events << event
+            state = event.state.deep_clone
+          end
         end
 
-        unless event.nil?
-          events << event
-          state = event.state.deep_clone
-        end
+        games << Game.new(events) unless events.empty?
       end
 
       games
@@ -30,9 +33,10 @@ module Australium
 
     # Parses a single line of TF2 log in the context of a game (if a {GameState} is passed).
     # @param [String] line the line to be parsed
-    # @param [GameState] the {GameState} containing the game context.
+    # @param [Integer] line_number the index of the line
+    # @param [GameState] state the {GameState} containing the game context.
     # @return [Event, NilClass] event if an event has been recognized; nil otherwise.
-    def self.parse_line(line, state = nil)
+    def self.parse_line(line, line_number, state = nil)
       Event::event_classes.each do |event_class|
         if event_class::LOG_REGEX =~ line
           # Get timestamp data & timestamp GameState if we are being stateful
@@ -47,9 +51,11 @@ module Australium
           data.merge!(property_data)
 
           # Add other useful data
+          data[:line_number] = line_number
           data[:state] = state
           data[:raw] = line
           data[:timestamp] = timestamp
+          data[:game_id] = state.game_id unless state.nil?
 
           # Construct and return the new Event
           return event_class.new(data)
